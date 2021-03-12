6635258555042387917e86e4f107559163486907
shrinivas12-mg <shrinivasgond8@gmail.com>
2021-03-12 15:00:28 +0530

    Addition of 3 number

--- Add.js
+++ Add.js
@@ -1,6 +1,6 @@
 const num1 = 5;
 const num2 = 3;
 
-const sum = num1 + num2;
+const sum = num1 + num2 + num3;
 
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+console.log('The sum of ' + num1 + ' and ' + num2 +'and '+num3+' is: ' + sum);
