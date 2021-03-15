614b2fcbcb39bac1719945ef13db1377ac378d46
shrinivas12-mg <shrinivasgond8@gmail.com>
2021-03-15 12:39:40 +0530

    Added comments

--- Add.js
+++ Add.js
@@ -1,7 +1,8 @@
 const num1 = 5;
 const num2 = 3;
 const num3 = 4;
-
+//Adding 3 numbers
 const sum = num1 + num2 + num3;
 
+//Output
 console.log('The sum of ' + num1 + ' and ' + num2 +'and '+num3+' is: ' + sum);
